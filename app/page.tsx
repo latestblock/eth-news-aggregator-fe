cea2db95c00bc842e859e51416e224f333923253
pavansargar <pavansargar36@gmail.com>
2025-04-18 01:19:28 +0530

    Add Prisma integration and update news components
    - Introduced Prisma client for database interactions.
    - Added .env file for database connection URL.
    - Updated package.json and yarn.lock to include Prisma dependencies.
    - Modified NewsItem and NewsDigest models in Prisma schema.
    - Enhanced news-list component to display news items with updated properties.
    - Implemented logic to fetch approved news digests in the Home component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
 import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
+import { NewsItem, NewsDigest } from "@prisma/client";
+import { DigestStatus } from "@prisma/client";
+import { NewsList } from "./components/news-list";
 
-export default function Home() {
+async function getApprovedDigests(): Promise<NewsDigest[]> {
+  const digests = await prisma.newsDigest.findMany({
+    where: {
+      status: "PENDING",
+    },
+    include: {
+      newsItems: true,
+    },
+    orderBy: {
+      date: "desc",
+    },
+  });
+  return digests;
+}
+
+export default async function Home() {
+  const digests = await getApprovedDigests();
+
   // Redirect to the latest news (you'll need to implement the logic to determine the latest date)
-  redirect("/news/2024/4/1");
+  // redirect("/news/2024/4/1");
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-6 px-6 pt-6">
+        Latest Ethereum News
+      </h1>
+      {digests.map((digest: any) => (
+        <NewsList key={digest?.id} items={digest?.newsItems} />
+      ))}
+    </div>
+  );
 }
