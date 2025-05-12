53fee429ba829cd95c6151e7d63bf548efd9c750
pavansargar <pavansargar36@gmail.com>
2025-05-12 23:57:14 +0530

    fix: loader

--- app/[chain]/loading.tsx
+++ app/[chain]/loading.tsx
@@ -1,8 +1,5 @@
 import { FullPageLoader } from "@/components/ui/loaders";
 
-// This is a server component, so we can't use hooks
-// The chain styling will be handled automatically in the loader
-// based on URL path detection
 const Loading = () => {
   return <FullPageLoader subtitle="Loading blockchain news & insights..." />;
 };
